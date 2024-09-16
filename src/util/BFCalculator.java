2626786da1216a70ef3044a20bd3269261341d57
Lily <blanchar2@grinnell.edu>
2024-09-16 16:49:31 -0500

    Add method comments

---
+++ src/util/BFCalculator.java
@@ -0,0 +1,60 @@
+public class BFCalculator {
+
+  // +--------+---------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /*
+   * The last calculated value.
+   */
+  BigFraction lastValue;
+
+  // +---------+--------------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /*
+   * Gets the last computed value.
+   * Returns 0 if there is no such value.
+   * 
+   * @return the last computed value
+   */
+  public BigFraction get() {
+    return 0;
+  } // get()
+
+  /*
+   * Adds val to the last computed value.
+   */
+  public void add(BigFraction val) {
+    return;
+  } // add(BigFraction)
+
+  /*
+   * Subtracts val from the last computed value.
+   */
+  public void subtract(BigFraction val) {
+    return;
+  } // subtract(BigFraction)
+
+  /*
+   * Multiplies the last computed value by val.
+   */
+  public void multiply(BigFraction val) {
+    return;
+  } // multiply(BigFraction)
+
+  /*
+   * Divides the last computed value by val.
+   */
+  public void divide(BigFraction val) {
+    return;
+  } // divide(BigFraction)
+
+  /*
+   * Resets the last computed value to 0.
+   */
+  public void clear() {
+    return;
+  } // clear()
+} // class BFCalculator
---
+++ src/util/BigFraction.java
@@ -0,0 +1,7 @@
+public class BigFraction {
+  //extended version of lab Fraction
+  //use BigInteger for num and denom
+  //Reduce all fractions all the way
+  //other constructors??
+  //other operations such as subtract and divide
+}
